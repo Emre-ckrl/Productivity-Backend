3f55f016fbfe78e40c67dfdb588e69b50943d7ce
Ilja <i.lichtenberg@tallence.com>
2020-03-12 12:35:41 +0100

    Backend service + APi Anbindung ans Backend Listen erstellen ToDos erstellen

--- src/main/java/cakiral/emre/projects/ToDo.java
+++ src/main/java/cakiral/emre/projects/ToDo.java
@@ -1,8 +1,14 @@
 package cakiral.emre.projects;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ToDo {
     public String text;
     public Human creator;
+    public boolean condition = false;
 
     public ToDo(String text, Human creator) {
         this.text = text;
